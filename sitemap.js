135928efb44452c7296a30f8b15db4ade528a191
yohankurniawan <49922329+yohankurniawan@users.noreply.github.com>
2019-06-03 05:52:30 +0700

    Add files via upload

---
+++ sitemap.js
@@ -0,0 +1,34 @@
+
+var numposts = 100;
+var standardstyling = true;
+
+/*  programmer and maker of this code is belong to founder of http://www.ardilas.com
+    rawgit url (https://cdn.rawgit.com/D-dig/js/gh-pages/sitemap3.js)
+*/
+
+function startpost(json){
+  for (var i = 0; i < numposts; i++){
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    
+    if (i == json.feed.entry.length) break;
+    
+    for (var k = 0; k < entry.link.length; k++){
+      if (entry.link[k].rel == 'alternate'){
+        posturl = entry.link[k].href;break;
+      }
+    }
+        
+    posttitle = posttitle.link(posturl);
+    if (standardstyling) document.write('<li>');
+    
+    document.write(posttitle);
+  }
+  
+  if (standardstyling) document.write('</li>');
+}
+
+function finished(){
+  document.write();
+}
